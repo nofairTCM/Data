6fe7437a66db264ad35eff298d0bcdeb97b9cc8d
살려주세요 <46598063+qwreey75@users.noreply.github.com>
2021-06-05 01:10:49 +0900

    버전업 부분 --verAdd 로 자동화

--- init.lua
+++ init.lua
@@ -21,22 +21,30 @@ local commands = {
         options = {
             ["--info"] = "info"; -- info file path
             ["--name"] = "name"; -- name of module
+            ["--verAdd"] = "verAdd";
         };
         execute = function (args,options)
             local info = json.decode(file.fileLoad(options.info));
             local list = json.decode(file.fileLoad("Package\\packageList.json"));
 
-            local item = list[options.name];
+            local verAdd = options.verAdd;
+            local name = options.name;
+            if verAdd then
+                info.publishVersion = info.publishVersion + 1;
+                info.version = ("%d.%d.%d"):format(info.majorVersion,info.publishVersion,info.buildVersion);
+            end
+
+            local item = list[name];
             for i,v in pairs(info) do
                 item[i] = v;
             end
 
-            file.fileSave("Package\\packageList.json",json.encode(list));
+            file.fileSave("Package\\packageList.json",json.encode(list,{indent = true}));
 
             os.execute(
                 "git config --global user.email \"41898282+github-actions[bot]@users.noreply.github.com\"" ..
                 "&&git config --global user.name \"github-actions[bot]\"" ..
-                ("&&git -C Package commit -m \"data base updated by github workflow (%s)\" -a"):format(options.name)
+                ("&&git -C Package commit -m \"data base updated by github workflow (%s)\" -a"):format(name)
             );
         end;
     };
